9f5b0e64d358c477d81a14d86d1003cf7c82d5c6
Einar Valgeirsson <einar.valgeirsson@jayway.com>
2017-02-14 23:23:03 +0100

    Splash screen now waits 2 secs before starting MainActivity

--- app/src/main/java/com/einarvalgeir/bussrapport/SplashActivity.java
+++ app/src/main/java/com/einarvalgeir/bussrapport/SplashActivity.java
@@ -1,15 +1,30 @@
 package com.einarvalgeir.bussrapport;
 
-import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
 
 
-public class SplashActivity extends Activity {
+public class SplashActivity extends AppCompatActivity {
+
+    private static final int WAIT_TIME_BEFORE_STARTING_APP_MILLIS = 2000;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash_layout);
+        waitUntilStartingApp();
+    }
 
+    private void waitUntilStartingApp() {
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        }, WAIT_TIME_BEFORE_STARTING_APP_MILLIS);
     }
 }
