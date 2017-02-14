163bd6763991f2cafe6cadacdfa71837f830f110
Einar Valgeirsson <einar.valgeirsson@jayway.com>
2017-02-14 23:03:51 +0100

    Initial commit

---
+++ app/src/main/java/com/einarvalgeir/bussrapport/SplashActivity.java
@@ -0,0 +1,15 @@
+package com.einarvalgeir.bussrapport;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+
+public class SplashActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_layout);
+
+    }
+}
