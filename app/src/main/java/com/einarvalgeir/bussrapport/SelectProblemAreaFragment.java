7e9249363f209e5831e6d4a36e07709e1d9df4c3
Einar Valgeirsson <einar.valgeirsson@jayway.com>
2017-02-22 21:14:32 +0100

    More transitions in SelectProblemAreaFragment

--- app/src/main/java/com/einarvalgeir/bussrapport/SelectProblemAreaFragment.java
+++ app/src/main/java/com/einarvalgeir/bussrapport/SelectProblemAreaFragment.java
@@ -1,17 +1,24 @@
 package com.einarvalgeir.bussrapport;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.transition.TransitionManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.OvershootInterpolator;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.einarvalgeir.bussrapport.model.Problem;
+import com.jakewharton.rxbinding.view.RxView;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -28,16 +35,14 @@ public class SelectProblemAreaFragment extends BaseFragment implements ProblemAr
     @BindView(R.id.problem_area_description)
     protected EditText problemAreaDescriptions;
 
+    @BindView(R.id.info_layout)
+    protected View infoLayout;
+
     protected ProblemAreasAdapter adapter;
     private LinearLayoutManager layoutManager;
 
     public static SelectProblemAreaFragment newInstance() {
         return new SelectProblemAreaFragment();
-    }
-
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
     }
 
     @Nullable
@@ -56,13 +61,43 @@ public class SelectProblemAreaFragment extends BaseFragment implements ProblemAr
         adapter = new ProblemAreasAdapter(getProblemAreasArray(), this);
         recyclerView.setItemAnimator(new FadeInAnimator(new OvershootInterpolator(1f)));
         recyclerView.setAdapter(adapter);
+        RxView.clicks(problemAreaName).subscribe(aVoid -> {
+            TransitionManager.beginDelayedTransition((ViewGroup) getView());
+
+            RelativeLayout.LayoutParams params = (RelativeLayout.LayoutParams) infoLayout.getLayoutParams();
+            params.addRule(RelativeLayout.BELOW, R.id.recycler_view);
+            problemAreaName.setAlpha(0);
+            infoLayout.requestLayout();
+        });
+
 
         return rootView;
     }
 
+
+    @SuppressLint("NewApi")
     @Override
     public void onProblemAreaSelected(ProblemAreasAdapter.ViewHolder vh, String name) {
+
+        TransitionManager.beginDelayedTransition((ViewGroup) getView());
+
+
         problemAreaName.setText(name);
+
+
+        RelativeLayout.LayoutParams params = (RelativeLayout.LayoutParams) infoLayout.getLayoutParams();
+        if (params == null) {
+            params = new RelativeLayout.LayoutParams(
+                    LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT);
+        }
+        params.removeRule(RelativeLayout.BELOW);
+        problemAreaName.setAlpha(1);
+
+        infoLayout.setLayoutParams(params);
+        infoLayout.requestLayout();
+        problemAreaDescriptions.requestFocus();
+        InputMethodManager imm = (InputMethodManager) getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.showSoftInput(problemAreaDescriptions, InputMethodManager.SHOW_IMPLICIT);
     }
 
     private String[] getProblemAreasArray() {
